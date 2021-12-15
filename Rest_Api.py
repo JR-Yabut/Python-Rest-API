f8ed37cf23829ec8ec6fa3d810ca53175f087855
noodleween <happyguy122x@gmail.com>
2021-12-15 10:59:53 -0700

    intial commit

---
+++ Rest_Api.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/roll', methods = ['GET', 'POST'])
+def dice():
+    count = int(request.args.get('count'))   # /roll?count=2
+    side = int(request.args.get('sides'))    # /roll?sides=20
+
+    if (not count): count = 1
+    if (not side): side = 20
+
+    results = []
+    number_die = list(range(count))
+    for d in number_die:
+        results.append(random.choice(list(range(1, side + 1))))
+
+    dic = {
+        "sides": side,
+        "count": count,
+        "results": results
+    }
+    
+    jsonData = jsonify(dic)
+
+    print(jsonData)
+
+    return jsonData
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 6969)
+
+
+# $ export FLASK_APP=hello
+# $ flask run
